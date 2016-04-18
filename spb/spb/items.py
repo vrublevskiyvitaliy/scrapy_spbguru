43a063bec546672369f0c87ca4b28b771a5b30ec
Vrublevskiy Vitaliy <vrublevskiy@lun.ua>
2016-04-18 16:17:59 +0300

    tmp

--- spb/spb/items.py
+++ spb/spb/items.py
@@ -10,24 +10,13 @@ import scrapy
 
 class SpbBuilding(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
     name = scrapy.Field()
-    region = scrapy.Field()
-    metro = scrapy.Field()
-    address = scrapy.Field()
-    date_of_ending = scrapy.Field()
-    building_class = scrapy.Field()
-    type_of_building = scrapy.Field()
-    number_of_floors = scrapy.Field()
-    amount_of_flats = scrapy.Field()
-    price = scrapy.Field()
-    payment_types = scrapy.Field()
     spbguru_href = scrapy.Field()
-    parking = scrapy.Field()
-    spbguru_company_href = scrapy.Field()
-    company_name = scrapy.Field()
-    permission_for_building = scrapy.Field()
+    # spbguru_company_href = scrapy.Field()
+
     spbguru_saler_href = scrapy.Field()
+    # saler_href
+
 
 
 class SpbCompany(scrapy.Item):
